34b2bc209c53070dda6962de4004a14cb1fa3008
Frederic Marchand <fred.marchand.23@gmail.com>
2014-09-24 17:38:33 -0400

    Everything working faster

--- main.cpp
+++ main.cpp
@@ -68,10 +68,23 @@ void medianFilter(int x1, int y1, int x2, int y2)
     }
     else
     {
-        cilk_spawn medianFilter(x1, y1, x2-(x2-x1)/2, y2-(y2-y1)/2); 
-        cilk_spawn medianFilter(x1+(x2-x1)/2, y1, x2, y2-(y2-y1)/2);
-        cilk_spawn medianFilter(x1, y1+(y2-y1)/2, x2-(x2-x1)/2, y2);
-        cilk_spawn medianFilter(x1+(x2-x1)/2, y1+(y2-y1)/2, x2, y2);
+        if (x2 - x1 == 1)
+        {
+            cilk_spawn medianFilter(x1, y1, x2, y2-(y2-y1)/2);
+            cilk_spawn medianFilter(x1, y1+(y2-y1)/2, x2, y2); 
+        }
+        else if (y2 - y1 == 1)
+        {
+            cilk_spawn medianFilter(x1, y1, x2-(x2-x1)/2, y2);
+            cilk_spawn medianFilter(x1+(x2-x1)/2, y1, x2, y2); 
+        }
+        else
+        {
+            cilk_spawn medianFilter(x1, y1, x2-(x2-x1)/2, y2-(y2-y1)/2); 
+            cilk_spawn medianFilter(x1+(x2-x1)/2, y1, x2, y2-(y2-y1)/2);
+            cilk_spawn medianFilter(x1, y1+(y2-y1)/2, x2-(x2-x1)/2, y2);
+            cilk_spawn medianFilter(x1+(x2-x1)/2, y1+(y2-y1)/2, x2, y2);
+        }
         cilk_sync;
     }
 }
